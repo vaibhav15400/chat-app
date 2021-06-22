c130faffaf894e46e3b7744c1142cd170c6d4b9a
vaibhav15400 <vaibhavmishrasurya@gmail.com>
2021-06-22 22:37:51 +0530

    managed user with profile content ; put real time subs

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import { Switch } from 'react-router-dom';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
 import SignIn from './pages/SignIn';
+import { ProfileProvider } from './Context/ProfileContext';
 import Home from './pages/Home';
 import PrivateRoute from './components/PrivateRoute';
 import PublicRoute from './components/PublicRoute';
 
 function App() {
   return (
-    <Switch>
-      <PublicRoute path="/signin">
-        <SignIn />
-      </PublicRoute>
-      <PrivateRoute path="/">
-        <Home />
-      </PrivateRoute>
-    </Switch>
+    <ProfileProvider>
+      <Switch>
+        <PublicRoute path="/signin">
+          <SignIn />
+        </PublicRoute>
+        <PrivateRoute path="/">
+          <Home />
+        </PrivateRoute>
+      </Switch>
+    </ProfileProvider>
   );
 }
 
--- src/components/PublicRoute.js
+++ src/components/PublicRoute.js
@@ -1,11 +1,21 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { Container, Loader } from 'rsuite';
 import { Route, Redirect } from 'react-router-dom';
+import { useProfile } from '../Context/ProfileContext';
 
 const PublicRoute = ({ children, ...routeProps }) => {
-  const profile = true;
+  const { profile, isLoading } = useProfile();
 
-  if (!profile) {
+  if (isLoading && !profile) {
+    return (
+      <Container>
+        <Loader center vertical size="md" content="LOADING" speed="slow" />
+      </Container>
+    );
+  }
+
+  if (profile && !isLoading) {
     return <Redirect to="/" />;
   }
 
