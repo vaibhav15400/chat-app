ac8e92eff7cadf9870ea8d9fb3be03b0265557bf
vaibhav15400 <vaibhavmishrasurya@gmail.com>
2021-06-23 18:51:04 +0530

    created rooms context

---
+++ src/Context/RoomContext.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+import React, { createContext, useEffect, useState } from 'react';
+import { database } from '../misc/firebase';
+import { transformToArrWithId } from '../misc/Helper';
+
+const RoomContext = createContext();
+
+export const RoomsProvider = ({ children }) => {
+  const [rooms, setRooms] = useState(null);
+
+  useEffect(() => {
+    const roomListRef = database.ref('rooms');
+
+    roomListRef.on('value', snap => {
+      const data = transformToArrWithId(snap.val());
+      setRooms(data);
+    });
+
+    return () => {
+      roomListRef.off();
+    };
+  }, []);
+  return <RoomContext.Provider value={rooms}>{children}</RoomContext.Provider>;
+};
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,19 @@
 import React from 'react';
 import { Grid, Col, Row } from 'rsuite';
 import Sidebar from '../components/Sidebar';
+import { RoomsProvider } from '../Context/RoomContext';
 
 const Home = () => {
   return (
-    <Grid fluid className="h-100">
-      <Row className='h-100'>
-        <Col xs={24} md={8} className='h-100'>
-          <Sidebar />
-        </Col>
-      </Row>
-    </Grid>
+    <RoomsProvider>
+      <Grid fluid className="h-100">
+        <Row className="h-100">
+          <Col xs={24} md={8} className="h-100">
+            <Sidebar />
+          </Col>
+        </Row>
+      </Grid>
+    </RoomsProvider>
   );
 };
 
