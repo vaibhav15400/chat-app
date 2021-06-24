e095c49e48cccb6ccc7bf5c2decb7a9303f3c134
vaibhav15400 <vaibhavmishrasurya@gmail.com>
2021-06-24 11:33:00 +0530

    added realtime listener for a user:

--- src/Context/ProfileContext.js
+++ src/Context/ProfileContext.js
@@ -1,6 +1,17 @@
 /* eslint-disable arrow-body-style */
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 const ProfileContext = createContext();
 
@@ -10,9 +21,10 @@ export const ProfileProvider = ({ children }) => {
 
   useEffect(() => {
     let userRef;
-
+    let userStatusRef;
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
 
         userRef.on('value', snap => {
@@ -23,25 +35,47 @@ export const ProfileProvider = ({ children }) => {
             avatar,
             uid: authObj.uid,
             email: authObj.email,
-
           };
           setProfile(data);
           setIsLoading(false);
+        });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (snapshot.val() === false) {
+            return;
+          }
+
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
         });
       } else {
         if (userRef) {
           userRef.off();
         }
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+
+        database.ref('.info/connected').off();
 
         setProfile(null);
         setIsLoading(false);
       }
     });
+
     return () => {
       authUnsub();
+      database.ref('.info/connected').off();
 
       if (userRef) {
         userRef.off();
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
       }
     };
   }, []);
--- src/components/Dashboard/DashboardToggle.js
+++ src/components/Dashboard/DashboardToggle.js
@@ -2,15 +2,27 @@
 import React, { useCallback } from 'react';
 import { Button, Icon, Drawer, Alert } from 'rsuite';
 import Dashboard from '.';
+import { isOfflineForDatabase } from '../../Context/ProfileContext';
 import { useMediaQuery, useModalState } from '../../misc/CustomHooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(max-width:992px)');
   const OnSignOut = useCallback(() => {
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        close();
+      })
+      .catch(error => {
+        Alert.error(error.message, 5000);
+      });
+
     auth.signOut();
-    Alert.info('SIGNED-OUT', 4000);
+    Alert.info('SIGNED-OUT', 5000);
     close();
   }, [close]);
 
